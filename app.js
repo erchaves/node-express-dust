3c22fac60a356981dfc41a822207ae985579e395
Eric Chaves <erchaves@gmail.com>
2017-01-09 17:25:31 -0500

    adding favicon
    and removing old dep

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -59,8 +58,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('pages/home', data);
 });
-
-// uncomment this after adding a favicon
-// app.use(favicon('./src/images/favicon.ico'));
 
 module.exports = app;
